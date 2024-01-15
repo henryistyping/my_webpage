5e29a402103f84ea4d041dc584337c49096d3e58
henryistyping <mykim1891@gmail.com>
2024-01-14 21:45:30 -0800

    basic GET and POST

--- backend/db.js
+++ backend/db.js
@@ -8,4 +8,5 @@ const pool = new Pool({
     database: "mywebsite"
 
 });
+
 module.exports = pool;
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,42 @@ const pool = require("./db");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
+
+//TODO: Routes
+// create a post
+
+app.post("/posts", async (req, res) => {
+    try {
+        console.log(req.body);
+        const { description } = req.body;
+        const newPost = await pool.query(
+            "INSERT INTO postlist (description) VALUES($1) RETURNING *",
+            [description]
+        );
+
+        res.json(newPost.rows[0])
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// get a list of all posts
+
+app.get("/posts", async (req, res) => {
+    try {
+        const allPosts = await pool.query(
+            "SELECT * FROM postlist"
+        );
+
+        res.json(allPosts.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+// get a post
+// update a post
+
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
