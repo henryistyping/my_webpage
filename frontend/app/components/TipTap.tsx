33e9c9048d9d71037bf1d7228b4ed13db976781d
henryistyping <mykim1891@gmail.com>
2024-01-15 20:23:19 -0800

    added all the buttons for editor

--- frontend/app/components/TipTap.tsx
+++ frontend/app/components/TipTap.tsx
@@ -20,6 +20,97 @@ const Tiptap = () => {
         <button onClick={() => editor.chain().focus().toggleBold().run()}>
           Bold
         </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}>
+          italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+        >
+          strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+        >
+          code
+        </button>
+        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+          clear marks
+        </button>
+        <button onClick={() => editor.chain().focus().clearNodes().run()}>
+          clear nodes
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+        >
+          paragraph
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        >
+          h1
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        >
+          h2
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        >
+          h3
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        >
+          h4
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        >
+          h5
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        >
+          h6
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+        >
+          bullet list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        >
+          ordered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        >
+          code block
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        >
+          blockquote
+        </button>
+        <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          horizontal rule
+        </button>
+        <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+          hard break
+        </button>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+        >
+          undo
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+        >
+          redo
+        </button>
       </div>
       <EditorContent editor={editor} />
     </>
