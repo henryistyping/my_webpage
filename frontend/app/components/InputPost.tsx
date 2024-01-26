f054f09bfc57446ffdd163f39a038fbea26462a6
henryistyping <mykim1891@gmail.com>
2024-01-26 13:43:15 -0800

    clean up + Making Tiptap single component

--- frontend/app/components/InputPost.tsx
+++ frontend/app/components/InputPost.tsx
@@ -2,13 +2,33 @@
 import React from 'react';
 
 import Tiptap from './TipTap';
-
+import { RichTextEditorProvider } from 'mui-tiptap';
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import CharacterCount from "@tiptap/extension-character-count";
+import { Button } from "@mui/material";
 
 const InputPost = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      CharacterCount.configure({
+        limit: 10000
+      }),],
+    content: "<p>Hello <b>world</b>!</p>",
+  });
+
+  const submit = editor?.getJSON();
+  
   return (
     <>
       <h1 className="">PERN POST</h1>
+      <RichTextEditorProvider editor={editor}>
         <Tiptap />
+        <Button onClick={() => console.log(submit)}>
+          Save
+        </Button>
+      </RichTextEditorProvider>
     </>
   )
 };
--- frontend/app/components/TipTap.tsx
+++ frontend/app/components/TipTap.tsx
@@ -1,5 +1,3 @@
-
-import { Button } from "@mui/material";
 import { useState, useRef } from "react"
 import {
   MenuButtonBold,
@@ -8,55 +6,30 @@ import {
   MenuDivider,
   MenuSelectHeading,
   RichTextEditorProvider,
-  type RichTextEditorRef,
   RichTextField,
+  useRichTextEditorContext,
 } from "mui-tiptap";
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import CharacterCount from "@tiptap/extension-character-count";
 
+//TODO: Use state management (context) to allow form submission in the parenet component OR figure out a way to pass down as prop to child component
 //TODO: Replace text with icons and make the buttons pretty
 //STRETCH: Create a Notion-style editor
 
 
 const Tiptap = () => {
-
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      CharacterCount.configure({
-        limit: 10000
-      }),],
-    content: "<p>Hello <b>world</b>!</p>",
-  });
-
-  const submit = editor?.getJSON();
-
-  const set_editorContent = useState("hello")
   return (
     <>
       {/* <form action="a certain url" method="post"> */}
-      <RichTextEditorProvider editor={editor}>
-        <RichTextField
-          controls={
-            <MenuControlsContainer>
-              <MenuSelectHeading />
-              <MenuDivider />
-              <MenuButtonBold />
-              <MenuButtonItalic />
-              {/* Add more controls of your choosing here */}
-            </MenuControlsContainer>
-          }
-        />
-      </RichTextEditorProvider>
-
-      {/* <Button type="submit" onClick={() => console.log(editor)}>
-          Save
-        </Button> */}
-      <Button onClick={() => console.log(submit)}>
-        Save
-      </Button>
-      {/* </form> */}
+      <RichTextField
+        controls={
+          <MenuControlsContainer>
+            <MenuSelectHeading />
+            <MenuDivider />
+            <MenuButtonBold />
+            <MenuButtonItalic />
+            {/* Add more controls of your choosing here */}
+          </MenuControlsContainer>
+        }
+      />
     </>
   )
 }
