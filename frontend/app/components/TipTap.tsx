caf67bfe056f61d9de8fb4458e021ab02485ff6c
henryistyping <mykim1891@gmail.com>
2024-01-29 20:24:55 -0800

    Submit Button placement

--- frontend/app/components/TipTap.tsx
+++ frontend/app/components/TipTap.tsx
@@ -7,9 +7,8 @@ import {
   RichTextField,
 } from "mui-tiptap";
 
-//TODO: Use state management (context) to allow form submission in the parenet component OR figure out a way to pass down as prop to child component
-//TODO: Replace text with icons and make the buttons pretty
-//STRETCH: Create a Notion-style editor
+//TODO: Finish incorporating menu buttons for the editor
+//STRETCH: Implement a Notion-style editor
 
 
 const Tiptap = () => {
