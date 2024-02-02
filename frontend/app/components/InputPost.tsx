4e5bc0987264538f9d05894700b5c473650ea0b1
henryistyping <mykim1891@gmail.com>
2024-02-02 14:06:01 -0800

    fix: implement React-hook-form with Tiptap

--- frontend/app/components/InputPost.tsx
+++ frontend/app/components/InputPost.tsx
@@ -6,9 +6,13 @@ import { RichTextEditorProvider } from 'mui-tiptap';
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import CharacterCount from "@tiptap/extension-character-count";
-import { Button } from "@mui/material";
+import { Button, FormControl } from "@mui/material";
+
+import { Controller, useForm } from 'react-hook-form';
 
 const InputPost = () => {
+  const { handleSubmit, control } = useForm()
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -21,15 +25,26 @@ const InputPost = () => {
   const submit = editor?.getJSON();
 
   //TODO: Implement a form that will send to DB upon button click `submit`
+  //TODO more specifically: find what properties should be passed down to Tiptap 
+  // refer to MUI/TIPTAP for all the properties that can be used, then rend them as fields for React-hook-form, and also implementing functions that go with them
   return (
     <>
       <h1 className="">PERN POST</h1>
-      <RichTextEditorProvider editor={editor}>
-        <Tiptap />
-        <Button onClick={() => console.log(submit)}>
-          Save
-        </Button>
-      </RichTextEditorProvider>
+      <FormControl onSubmit={handleSubmit(data => console.log(data))}>
+        <Controller
+          control={control}
+          name="TipTap"
+          render={({ field: onChange, value, ref }) => ( //Look at the tip section
+            <RichTextEditorProvider onChange={onChange} selected={value} editor={editor}>
+              <Tiptap />
+              <Button onClick={() => console.log(submit)}>
+                Save
+              </Button>
+            </RichTextEditorProvider>
+          )}
+        />
+
+      </FormControl>
     </>
   )
 };
